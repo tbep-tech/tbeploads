4420a3772640888fbfff4c854b03b223ac27d5ee
fawda123 <mbafs2012@gmail.com>
2024-03-19 09:41:58 -0400

    filling out test coverage for util_dps_checkfls

--- tests/testthat/test-util_dps_checkfls.R
+++ tests/testthat/test-util_dps_checkfls.R
@@ -9,6 +9,30 @@ test_that("Check output class", {
 
 test_that("Check chk entries", {
 
-  expect_true(all(chkfls$chk %in% c('ok', 'read error', 'check columns')))
+  expect_true(all(chkfls$chk %in% 'ok'))
+
+})
+
+test_that("Check file incorrect columns", {
+
+  tmpfl <- tempfile(fileext = '.txt')
+  write.table(NA, tmpfl)
+  chkfls <- util_dps_checkfls(dirname(tmpfl))
+
+  expect_true(chkfls$chk == 'check columns')
+
+  file.remove(tmpfl)
+
+})
+
+test_that("Check file read error", {
+
+  tmpfl <- tempfile(fileext = '.txt')
+  writeLines(c("header1\theader2\theader3", "1\t2\t3", "a\tb"), tmpfl)
+  chkfls <- util_dps_checkfls(dirname(tmpfl))
+
+  expect_true(chkfls$chk == 'read error')
+
+  file.remove(tmpfl)
 
 })
