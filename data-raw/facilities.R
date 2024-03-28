b173875bb069843839cf57cbdca086c0d0408968
fawda123 <mbafs2012@gmail.com>
2024-03-28 11:28:42 -0400

    facilites source data to data-raw

--- data-raw/facilities.R
+++ data-raw/facilities.R
@@ -1,14 +1,15 @@
 library(haven)
 library(dplyr)
 library(tibble)
+library(here)
 
-pth <- 'T:/03_BOARDS_COMMITTEES/05_TBNMC/2022_RA_Update/01_FUNDING_OUT/DELIVERABLES/TO-9/datastick_deliverables/2017-2021LUEntityLoads/'
-
-ipsfac <- read_sas(paste0(pth, '2017-2021IPSMonthlyEntityBasin/ips1721monthentbas.sas7bdat')) |>
+# original T:/03_BOARDS_COMMITTEES/05_TBNMC/2022_RA_Update/01_FUNDING_OUT/DELIVERABLES/TO-9/datastick_deliverables/2017-2021LUEntityLoads/2017-2021IPSMonthlyEntityBasin/ips1721monthentbas.sas7bdat
+ipsfac <- read_sas(here('data-raw/ips1721monthentbas.sas7bdat')) |>
   select(bayseg, basin = BASIN, entity, facname, source = source2) |>
   distinct()
 
-dpsfac <- read_sas(paste0(pth, '2017-2021DPSMonthlyEntityBasin/dps1721monthentbas.sas7bdat')) |>
+# original T:/03_BOARDS_COMMITTEES/05_TBNMC/2022_RA_Update/01_FUNDING_OUT/DELIVERABLES/TO-9/datastick_deliverables/2017-2021LUEntityLoads/2017-2021DPSMonthlyEntityBasin/dps1721monthentbas.sas7bdat
+dpsfac <- read_sas(here('data-raw/dps1721monthentbas.sas7bdat')) |>
   select(bayseg, basin, entity, facname, source = source2) |>
   distinct() |>
   mutate( # make ototw its own entity
