2f1c131ae8a75c1235398d80736c91945f5dac38
fawda123 <mbafs2012@gmail.com>
2024-06-28 16:32:38 -0400

    ad_rain file has station as numeric, ready for all ad load calc

--- R/util_ad_getrain.R
+++ R/util_ad_getrain.R
@@ -10,17 +10,43 @@
 #'
 #' @details This function is used to retrieve a long-term record of rainfall for estimating AD loads.  It is used to create an input data file for load calculations and it is not used directly by any other functions due to download time.  A NOAA API key is required to use the function.
 #'
-#' By default, rainfall data is retrieved for the stations 228, 478, 520, 940, 945, 1046, 1163, 1632, 1641, 2806, 3153, 3986, 4707, 5973, 6065, 6880, 7205, 7851, 7886, 8788, 8824, 9176, and 9401.
+#' By default, rainfall data is retrieved for the following stations:
+#'
+#' \itemize{
+#'    \item \code{228}: ARCADIA
+#'    \item \code{478}: BARTOW
+#'    \item \code{520}: BAY LAKE
+#'    \item \code{940}: BRADENTON EXPERIMENT
+#'    \item \code{945}: BRADENTON 5 ESE
+#'    \item \code{1046}: BROOKSVILLE CHIN HIL
+#'    \item \code{1163}: BUSHNELL 2 E
+#'    \item \code{1632}: CLEARWATER
+#'    \item \code{1641}: CLERMONT 7 S
+#'    \item \code{2806}: ST PETERSBURG WHITTD
+#'    \item \code{3153}: FORT GREEN 12 WSW
+#'    \item \code{3986}: HILLSBOROUGH RVR SP
+#'    \item \code{4707}: LAKE ALFRED EXP STN
+#'    \item \code{5973}: MOUNTAIN LAKE
+#'    \item \code{6065}: MYAKKA RIVER STATE P
+#'    \item \code{6880}: PARRISH
+#'    \item \code{7205}: PLANT CITY
+#'    \item \code{7851}: ST LEO
+#'    \item \code{7886}: ST PETERSBURG WHITTD
+#'    \item \code{8788}: TAMPA INTL ARPT
+#'    \item \code{8824}: TARPON SPNGS SWG PLT
+#'    \item \code{9176}: VENICE
+#'    \item \code{9401}: WAUCHULA 2 N
+#' }
 #'
 #' @return a data frame with the following columns:
 #'
 #' \itemize{
-#'  \item{stationid}{character, the station id}
-#'  \item{date}{Date, the date of the observation}
-#'  \item{Year}{numeric, the year of the observation}
-#'  \item{Month}{numeric, the month of the observation}
-#'  \item{Day}{numeric, the day of the observation}
-#'  \item{rainfall}{numeric, the amount of rainfall in inches}
+#'  \item \code{station}: numeric, the station id
+#'  \item \code{date}: Date, the date of the observation
+#'  \item \code{Year}: numeric, the year of the observation
+#'  \item \code{Month}: numeric, the month of the observation
+#'  \item \code{Day}: numeric, the day of the observation
+#'  \item \code{rainfall}: numeric, the amount of rainfall in inches
 #'  }
 #'
 #' @export
@@ -82,14 +108,15 @@ util_ad_getrain <- function(yrs, station = NULL, noaa_key, ntry = 5, quiet = FAL
     tidyr::unnest('data') |>
     tidyr::unnest('data') |>
     dplyr::mutate(
+      station = as.numeric(gsub('GHCND:USC0008', '', stationid)),
       date = lubridate::date(date),
       Year = yrs,
       Month = lubridate::month(date),
       Day = lubridate::day(date),
       rainfall = round(value / 254, 2)
       ) %>%
-    dplyr::select(stationid, date, Year, Month, Day, rainfall) |>
-    dplyr::arrange(stationid, date)
+    dplyr::select(station, date, Year, Month, Day, rainfall) |>
+    dplyr::arrange(station, date)
 
   return(out)
 
