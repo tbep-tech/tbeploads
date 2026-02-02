c3daf48d26d9fe697ffb8ddb5d8e464ea9681d0a
fawda123 <mbafs2012@gmail.com>
2026-02-02 16:21:25 -0500

    comment clucs conversion in util_nps_tbbase processing for now

--- R/util_nps_landsoil.R
+++ R/util_nps_landsoil.R
@@ -22,11 +22,11 @@ util_nps_landsoil <- function(tbbase){
         hydgrp == "B/D" ~ "B",
         hydgrp == "C/D" ~ "C",
         TRUE ~ hydgrp
-      ), 
-      CLUCSID = dplyr::case_when(
-        CLUCSID == 10 ~ 14, # manual fix to clucsid 10 to 14, per line 39 in RP code 11_F3D_2224_25Sep25.SAS at T:\03_BOARDS_COMMITTEES\05_TBNMC\TB_LOADS\2027_RA_Deliverables\2224\Extra from RP 20260109
-        TRUE ~ CLUCSID
-      )
+      )#, 
+      # CLUCSID = dplyr::case_when(
+      #   CLUCSID == 10 ~ 14, # manual fix to clucsid 10 to 14, per line 39 in RP code 11_F3D_2224_25Sep25.SAS at T:\03_BOARDS_COMMITTEES\05_TBNMC\TB_LOADS\2027_RA_Deliverables\2224\Extra from RP 20260109
+      #   TRUE ~ CLUCSID
+      # )
     ) |>
     dplyr::rename(
       clucsid = CLUCSID,
